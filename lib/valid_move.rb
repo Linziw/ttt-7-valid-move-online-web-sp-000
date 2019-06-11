7f75d81e67390d815f118a277323f190c19e9624
Lindsey Wells <lindsey.wells84@gmail.com>
2019-06-11 12:55:14 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 # code your #valid_move? method here
 def valid_move?
-  if !position_taken?(board, index) && index.between?(1,9)
+  if !position_taken?(board, index) && index.between?(0,8)
     true
  
   end
